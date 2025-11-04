96e2c74f4676acaf6b687cf24a7a6a07f36f1b42
TonyMorse92 <anthonymorse92@gmail.com>
2025-11-04 10:42:06 -0500

    Putting plotting in function.

--- mean.py
+++ mean.py
@@ -4,6 +4,11 @@ import numpy as np
 import random as rand
 import matplotlib.pyplot as plt
 
+def plot_hist(x: List[float]) -> None:
+	plt.hist(timing, bins=25)
+	plt.title("Hist with 25 bins.")
+	plt.show()
+
 def my_mean(x: List[int]) -> None:
 	return sum(x)/len(x)
 
@@ -12,7 +17,7 @@ num_runs = 1000
 timing = num_runs * [0]
 
 for i in range(num_runs):
-	test_array = rand.choices(range(1000), k=100000)
+	test_array = rand.choices(range(1000), k=1000000)
 	start = time.perf_counter()
 	my_mean(test_array)
 	end = time.perf_counter()
@@ -24,8 +29,5 @@ print(f"On average, it took {my_mean(timing):.8f} seconds.")
 
 
 
-plt.hist(timing, bins=25)
+plot_hist(timing)
 
-plt.title("Hist with 25 bins.")
-
-plt.show()
