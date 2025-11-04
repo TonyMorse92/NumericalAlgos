89a3d8bdeaa641cd452bbe865b73037c0744f27c
TonyMorse92 <anthonymorse92@gmail.com>
2025-11-03 19:23:27 -0500

    Added some trial runs and basic historgram plot.

---
+++ mean.py
@@ -0,0 +1,31 @@
+from typing import List
+import time
+import numpy as np
+import random as rand
+import matplotlib.pyplot as plt
+
+def my_mean(x: List[int]) -> None:
+	return sum(x)/len(x)
+
+num_runs = 1000
+
+timing = num_runs * [0]
+
+for i in range(num_runs):
+	test_array = rand.choices(range(1000), k=100000)
+	start = time.perf_counter()
+	my_mean(test_array)
+	end = time.perf_counter()
+	time_taken = end - start
+	timing[i] = time_taken
+
+
+print(f"On average, it took {my_mean(timing):.8f} seconds.")
+
+
+
+plt.hist(timing, bins=25)
+
+plt.title("Hist with 25 bins.")
+
+plt.show()
