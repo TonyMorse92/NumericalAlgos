4eb7d8044a708bb2846e7548de7ca7b3e8b04860
TonyMorse92 <anthonymorse92@gmail.com>
2025-12-05 09:55:28 -0500

    Adding random walk.

---
+++ walk.py
@@ -0,0 +1,27 @@
+import random
+
+def random_walk(n: int) -> tuple:
+	x,y = 0,0
+	for step in range(n):
+		(dx,dy) = random.choice([(0,1),(0,-1),(1,0),(-1,0)])
+		x += dx
+		y += dy
+	return (x,y)
+
+def walk(num_trials: int):
+	num_walks = 100000
+
+	for walk_length in range(1, num_trials + 1):
+		# Number of walks that keep you close to home 
+		local = 0
+		for i in range(num_walks):
+			(x,y) = random_walk(walk_length)
+			distance = abs(x) + abs(y)
+			local_distance = 5
+			if distance <= local_distance:
+				local += 1
+		close_walks = float(local)/float(num_walks)
+		print(f"Walk size = {walk_length} / % of close walks = {100*close_walks}")
+
+
+walk(50)
