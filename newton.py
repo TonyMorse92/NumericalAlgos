23838fa10ce10c28878474eb46027cc09f443a98
TonyMorse92 <anthonymorse92@gmail.com>
2025-12-06 19:17:58 -0500

    Root finding: Newton's method.

---
+++ newton.py
@@ -0,0 +1,22 @@
+def newton(f, der_f, x0, eps, iterations):
+	xn = x0
+	for n in range(0, iterations):
+		f_xn = f(xn)
+		if abs(f_xn) < eps:
+			print(f"Found solution after {n} iterations.")
+			return xn
+		der_f_xn = der_f(xn)
+		if der_f_xn == 0:
+			print(f"No solution found.")
+			return None
+		xn = xn - (f_xn/der_f_xn)
+	print(f"Exceeded maximum iterations. No solution found.")
+	return None
+	    
+def test():
+	f = lambda x: x**2 - x - 1
+	der_f = lambda x: 2*x - 1
+	print(f"This soultion should be about 1.61: {newton(f, der_f, 1, 1e-4, 10)}") 
+
+
+test()
