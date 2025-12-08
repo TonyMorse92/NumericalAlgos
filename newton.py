0363d4dec25df4ea2fd12a8a0eeffe0df0cf1b55
TonyMorse92 <anthonymorse92@gmail.com>
2025-12-07 23:59:11 -0500

    Changing test.

--- newton.py
+++ newton.py
@@ -14,9 +14,9 @@ def newton(f, der_f, x0, eps, iterations):
 	return None
 	    
 def test():
-	f = lambda x: x**2 - x - 1
-	der_f = lambda x: 2*x - 1
-	print(f"This soultion should be about 1.61: {newton(f, der_f, 1, 1e-4, 10)}") 
+	f = lambda x: x**3 - x - 1
+	der_f = lambda x: 3*x**2 - 1
+	print(f"This soultion should be about 1.32: {newton(f, der_f, 1, 1e-4, 10)}") 
 
 
 test()
