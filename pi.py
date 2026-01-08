ec7030c6216227d69749952e63e9232dcd8e50bb
TonyMorse92 <anthonymorse92@gmail.com>
2026-01-07 21:08:50 -0500

    Better var naming.

--- pi.py
+++ pi.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 # Number of "darts"
-N = 100000
+N = 10000
 
 # Number of "darts" landing in circle
-in_circle = 0
+num_in_circle = 0
 
 # We need to pick random x coordinate and random y coordinate. 
 # Add if in.
@@ -13,10 +13,10 @@ for i in range (N-1):
     y = np.random.uniform(-1,1)
     
     if(x**2 + y**2 <= 1):
-        in_circle += 1
+        num_in_circle += 1
         
 print(f"There were {in_circle} darts that landed in the circle.")
 
 # Pi is approximately 4 * (in_circle / N)
-pi_approx = 4 * ((in_circle)/(N))
+pi_approx = 4 * ((num_in_circle)/(N))
 print(f"Pi is approximately: {pi_approx}")
